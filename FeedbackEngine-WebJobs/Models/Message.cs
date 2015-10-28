b67ada38963ec6c56ac43cb976621c89cac4dac0
Chris Anderson <chrande@outlook.com>
2015-10-27 23:44:46 -0700

    Message to slack working. Rate limiting still TBD.

---
+++ FeedbackEngine-WebJobs/Models/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeedbackEngine_WebJobs.Models
+{
+    public class Message
+    {
+        public string to { get; set; }
+        public string contents { get; set; }
+    }
+}
